126f791562dfac09ab03a717d81d57c435e3dc8c
balavignesh6779 <balavigneshp06@gmail.com>
2026-03-15 13:38:19 +0530

    Add files via upload

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class OopsBannerApp {
     public static void main(String[] args) {
 
-        // Create Scanner object to take input from user
+        // Create Scanner object to get input from user
         Scanner sc = new Scanner(System.in);
 
         // Ask user to enter a number
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a natural number: ");
         int number = sc.nextInt();
 
-        // Using for loop to print multiplication table from 6 to 9
-        for (int i = 6; i <= 9; i++) {
+        // Check if the number is a natural number
+        if (number <= 0) {
+            System.out.println("Please enter a natural number greater than 0.");
+        } 
+        else {
 
-            // Print in the required format
-            System.out.println(number + " * " + i + " = " + (number * i));
+            // Loop from 1 to the entered number
+            for (int i = 1; i <= number; i++) {
+
+                // Check if the number is even or odd
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } 
+                else {
+                    System.out.println(i + " is Odd");
+                }
+            }
         }
 
         // Close scanner
